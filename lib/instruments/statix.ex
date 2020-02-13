713d246ed2bb2237605cc7dd107333d9e2cd9311
Roman Heinrich <roman.heinrich@gmail.com>
2020-02-13 22:48:58 +0100

    Make Statix module optional

--- lib/instruments/statix.ex
+++ lib/instruments/statix.ex
@@ -1,6 +1,8 @@
-defmodule Instruments.Statix do
-  @moduledoc """
-  The default stats reporter. Uses the `Statix` library.
-  """
-  use Statix, runtime_config: true
+if function_exported?(Statix, :__info__, 1) do
+  defmodule Instruments.Statix do
+    @moduledoc """
+    The default stats reporter. Uses the `Statix` library.
+    """
+    use Statix, runtime_config: true
+  end
 end
